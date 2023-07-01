4965b9bf0a82a9864e0f462f7ebd0857ae7387bb
bhagyaj <bhagyajkumar1@gmail.com>
2023-07-02 00:43:46 +0530

    updated host

--- dsawiz/settings.py
+++ dsawiz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://dsawizapi.onrender.com"]
 
 
 # Application definition
