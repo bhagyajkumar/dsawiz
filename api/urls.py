c4d5da2db95ee6606f070263e2b8895e15b9014d
bhagyaj <bhagyajkumar1@gmail.com>
2023-07-01 17:39:26 +0530

    feat: Registration, login and execute code

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,16 @@
 from . import views
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path("api/execute", views.ExecuteCode.as_view(), name="execute code")
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', views.RegisterUser.as_view(),name="create_user"),
+    path("api/execute", views.ExecuteCode.as_view(), name="execute_code"),
+    
 ]
