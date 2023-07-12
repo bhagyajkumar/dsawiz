4efc4efe3b987b0115913b7a3ae00bd1de03a6f3
bhagyaj <bhagyajkumar1@gmail.com>
2023-07-12 22:59:44 +0530

    feat: now post will have user object

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ExecuteCodeSerializer(serializers.Serializer):
     runtime = serializers.CharField(max_length=100)
     version = serializers.CharField(max_length=10)
     code = serializers.CharField()
+
 
 class UserRegistrationSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -15,11 +19,20 @@ class UserRegistrationSerializer(serializers.Serializer):
         ret = super().to_representation(instance)
         ret.pop('password')
         return ret
-    
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username"]
+
+
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Post
-        fields = ['id','title', 'user', 'image']
+        fields = ['id', 'title', 'user', 'image']
+
 
 class PostSerializerCreate(serializers.ModelSerializer):
     class Meta:
--- api/urls.py
+++ api/urls.py
@@ -12,10 +12,10 @@ router = routers.DefaultRouter()
 router.register(r'api/post', views.PostViewSet, basename='post')
 
 urlpatterns = [
-    path("", include("rest_framework.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', views.RegisterUser.as_view(),name="create_user"),
     path("api/execute", views.ExecuteCode.as_view(), name="execute_code"),
-     path('', include(router.urls)),
+    path('', include(router.urls)),
+    path("", include("rest_framework.urls")),
 ]
